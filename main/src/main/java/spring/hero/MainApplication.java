65c8cffed8fc519f130cf389613e22f411ad12d3
pv_ 398 <trungpv398@gmail.com>
2021-12-01 14:23:40 +0700

    updaye get mapping

--- main/src/main/java/spring/hero/MainApplication.java
+++ main/src/main/java/spring/hero/MainApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring.hero.entity.model.Doctor;
@@ -20,7 +21,7 @@ public class MainApplication {
         SpringApplication.run(MainApplication.class,args);
 
     }
-    @RequestMapping
+    @GetMapping
     public Doctor index()
     {
         Doctor doctor = new Doctor(1,"hello");
